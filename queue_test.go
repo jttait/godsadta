1efabb672f45383500617bd1c8d11b2d02c538dd
Jonathon Tait <jttait@gmail.com>
2023-08-27 20:04:18 +0100

    generic queue

--- queue_test.go
+++ queue_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestShouldBeSizeZeroForNewlyInstantiatedQueue(t *testing.T) {
 	want := 0
-	q := NewQueue()
+	q := NewQueue[int]()
 	result := q.Size()
 	if want != result {
 		t.Fatalf("Want %v. Got %v\n", want, result)
@@ -13,7 +13,7 @@ func TestShouldBeSizeZeroForNewlyInstantiatedQueue(t *testing.T) {
 
 func TestShouldBeSizeOneWhenItemAddedToNewlyInstantiatedQueue(t *testing.T) {
 	want := 1
-	q := NewQueue()
+	q := NewQueue[int]()
 	q.Add(5)
 	result := q.Size()
 	if want != result {
@@ -23,7 +23,7 @@ func TestShouldBeSizeOneWhenItemAddedToNewlyInstantiatedQueue(t *testing.T) {
 
 func TestShouldRemoveItemFromQueueOfSizeOne(t *testing.T) {
 	want := 5
-	q := NewQueue()
+	q := NewQueue[int]()
 	q.Add(5)
 	result, _ := q.Remove()
 	if want != result {
@@ -33,7 +33,7 @@ func TestShouldRemoveItemFromQueueOfSizeOne(t *testing.T) {
 
 func TestShouldBeFalseWhenRemovingFromEmptyQueue(t *testing.T) {
 	want := false
-	q := NewQueue()
+	q := NewQueue[int]()
 	_, ok := q.Remove()
 	if want != ok {
 		t.Fatalf("Want %v. Got %v\n", want, ok)
@@ -42,7 +42,7 @@ func TestShouldBeFalseWhenRemovingFromEmptyQueue(t *testing.T) {
 
 func TestShouldBeTrueWhenRemovingFromNonEmptyQueue(t *testing.T) {
 	want := true
-	q := NewQueue()
+	q := NewQueue[int]()
 	q.Add(5)
 	_, ok := q.Remove()
 	if want != ok {
@@ -50,11 +50,22 @@ func TestShouldBeTrueWhenRemovingFromNonEmptyQueue(t *testing.T) {
 	}
 }
 
-func TestShouldRemoveItemThatWasAddedEarliest(t *testing.T) {
+func TestShouldRemoveIntegerThatWasAddedEarliest(t *testing.T) {
 	want := 5
-	q := NewQueue()
+	q := NewQueue[int]()
 	q.Add(5)
 	q.Add(6)
+	result, _ := q.Remove()
+	if want != result {
+		t.Fatalf("Want %v. Got %v\n", want, result)
+	}
+}
+
+func TestShouldRemoveStringThatWasAddedEarliest(t *testing.T) {
+	want := "five"
+	q := NewQueue[string]()
+	q.Add("five")
+	q.Add("six")
 	result, _ := q.Remove()
 	if want != result {
 		t.Fatalf("Want %v. Got %v\n", want, result)
