dc64b3a55985f762426ca24cf231ec4504ed3aff
Jonathon Tait <jttait@gmail.com>
2023-08-28 17:48:36 +0100

    add min heap

--- minheap/min_heap.go
+++ minheap/min_heap.go
@@ -1,18 +1,18 @@
-// Package maxheap provides the max heap data structure and associated methods
-package maxheap
+// Package minheap provides the min heap data structure and associated methods
+package minheap
 
 import (
 	"cmp"
 )
 
-type MaxHeap[T cmp.Ordered] struct {
+type MinHeap[T cmp.Ordered] struct {
 	array []T
 }
 
-// NewMaxHeap instantiates a max heap and returns a pointer to it. The variadic argument can be used
+// NewMinHeap instantiates a min heap and returns a pointer to it. The variadic argument can be used
 // to insert items into the heap after it is created.
-func NewMaxHeap[T cmp.Ordered](i ...T) *MaxHeap[T] {
-	m := MaxHeap[T]{}
+func NewMinHeap[T cmp.Ordered](i ...T) *MinHeap[T] {
+	m := MinHeap[T]{}
 	for _, j := range i {
 		m.Insert(j)
 	}
@@ -26,12 +26,12 @@ func findParentIndex(index int) int {
 	return (index - 1) / 2
 }
 
-// Insert inserts the item into the max heap.
-func (m *MaxHeap[T]) Insert(i T) {
+// Insert inserts the item into the min heap.
+func (m *MinHeap[T]) Insert(i T) {
 	m.array = append(m.array, i)
 	index := len(m.array) - 1
 	parentIndex := findParentIndex(index)
-	for parentIndex >= 0 && m.array[index] > m.array[parentIndex] {
+	for parentIndex >= 0 && m.array[index] < m.array[parentIndex] {
 		temp := m.array[index]
 		m.array[index] = m.array[parentIndex]
 		m.array[parentIndex] = temp
@@ -40,9 +40,9 @@ func (m *MaxHeap[T]) Insert(i T) {
 	}
 }
 
-// Extract returns the maximum item from the max heap and swaps the elements as necessary to
+// Extract returns the minimum item from the min heap and swaps the elements as necessary to
 // restore the heap property in the data structure.
-func (m *MaxHeap[T]) Extract() T {
+func (m *MinHeap[T]) Extract() T {
 	result := m.array[0]
 	m.array[0] = m.array[len(m.array)-1]
 	m.array = m.array[:len(m.array)-1]
@@ -50,8 +50,8 @@ func (m *MaxHeap[T]) Extract() T {
 	index := 0
 	childIndex1 := (index * 2) + 1
 	childIndex2 := (index * 2) + 2
-	for index < len(m.array) && ((childIndex1 < len(m.array) && m.array[index] < m.array[childIndex1]) || (childIndex2 < len(m.array) && m.array[index] < m.array[childIndex2])) {
-		if m.array[childIndex1] > m.array[childIndex2] {
+	for index < len(m.array) && ((childIndex1 < len(m.array) && m.array[index] > m.array[childIndex1]) || (childIndex2 < len(m.array) && m.array[index] > m.array[childIndex2])) {
+		if m.array[childIndex1] < m.array[childIndex2] {
 			temp := m.array[childIndex1]
 			m.array[childIndex1] = m.array[index]
 			m.array[index] = temp
