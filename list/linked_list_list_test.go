50cfef6599d6fd32a085473f510867b116e5a2eb
Jonathon Tait <jttait@gmail.com>
2023-09-03 20:09:43 +0100

    refactor linked list list test

--- list/linked_list_list_test.go
+++ list/linked_list_list_test.go
@@ -5,10 +5,7 @@ import "testing"
 func TestShouldInstantiateLinkedListListOfSizeZero(t *testing.T) {
 	l := NewLinkedListList[int]()
 	result := l.Size()
-	want := 0
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 0, t)
 }
 
 func TestShouldAppendMultipleItemsToLinkedListList(t *testing.T) {
@@ -17,42 +14,24 @@ func TestShouldAppendMultipleItemsToLinkedListList(t *testing.T) {
 	l.Append(6)
 	l.Append(7)
 	result, _ := l.Get(0)
-	want := 5
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 5, t)
 	result, _ = l.Get(1)
-	want = 6
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 6, t)
 	result, _ = l.Get(2)
-	want = 7
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 7, t)
 }
 
 func TestShouldIncreaseSizeWhenAppendingToLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(5)
 	result := l.Size()
-	want := 1
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 1, t)
 	l.Append(6)
 	result = l.Size()
-	want = 2
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 2, t)
 	l.Append(7)
 	result = l.Size()
-	want = 3
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 3, t)
 }
 
 func TestShouldPrependMultipleItemsToLinkedListList(t *testing.T) {
@@ -61,68 +40,44 @@ func TestShouldPrependMultipleItemsToLinkedListList(t *testing.T) {
 	l.Prepend(6)
 	l.Prepend(7)
 	result, _ := l.Get(0)
-	want := 7
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 7, t)
 	result, _ = l.Get(1)
-	want = 6
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 6, t)
 	result, _ = l.Get(2)
-	want = 5
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 5, t)
 }
 
 func TestShouldIncreaseSizeWhenPrependingToLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Prepend(5)
 	result := l.Size()
-	want := 1
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 1, t)
 	l.Prepend(6)
 	result = l.Size()
-	want = 2
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 2, t)
 	l.Prepend(7)
 	result = l.Size()
-	want = 3
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 3, t)
 }
 
 func TestShouldBeFalseIfGetOnEmptyLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	_, ok := l.Get(0)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldBeTrueIfGetIndexIsWithinLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(5)
 	_, ok := l.Get(0)
-	if !ok {
-		t.Fatalf("Got false. Want true.")
-	}
+	assertTrue(ok, t)
 }
 
 func TestShouldBeFalseIfGetIndexIsOutsideLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(5)
 	_, ok := l.Get(1)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldRemoveItemAtIndexOfLinkedListList(t *testing.T) {
@@ -132,41 +87,30 @@ func TestShouldRemoveItemAtIndexOfLinkedListList(t *testing.T) {
 	l.Append(3)
 	_ = l.Remove(1)
 	result, _ := l.Get(1)
-	want := 3
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 3, t)
 	_ = l.Remove(1)
 	_, ok := l.Get(1)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldBeFalseWhenRemovingFromEmptyLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	ok := l.Remove(0)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldBeFalseWhenRemovingFromIndexOutsideLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(5)
 	ok := l.Remove(1)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldBeTrueWhenRemovingFromIndexInsideLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(5)
 	ok := l.Remove(0)
-	if !ok {
-		t.Fatalf("Got false. Want true.")
-	}
+	assertTrue(ok, t)
 }
 
 func TestShouldInsertItemAtIndexOfLinkedListList(t *testing.T) {
@@ -174,10 +118,7 @@ func TestShouldInsertItemAtIndexOfLinkedListList(t *testing.T) {
 	l.Append(1)
 	_ = l.Insert(0, 2)
 	result, _ := l.Get(0)
-	want := 2
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 2, t)
 }
 
 func TestShouldShiftTheItemCurrentlyAtTheIndexToRightWithLinkedListList(t *testing.T) {
@@ -185,36 +126,27 @@ func TestShouldShiftTheItemCurrentlyAtTheIndexToRightWithLinkedListList(t *testi
 	l.Append(1)
 	_ = l.Insert(0, 2)
 	result, _ := l.Get(1)
-	want := 1
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 1, t)
 }
 
 func TestShouldBeFalseWhenInsertingIntoEmptyLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	ok := l.Insert(0, 1)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldBeTrueWhenInsertingIntoLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(1)
 	ok := l.Insert(0, 2)
-	if !ok {
-		t.Fatalf("Got false. Want true.")
-	}
+	assertTrue(ok, t)
 }
 
 func TestShouldBeFalseWhenInsertingIntoIndexOutsideLinkedListList(t *testing.T) {
 	l := NewArrayList[int]()
 	l.Append(1)
 	ok := l.Insert(1, 2)
-	if ok {
-		t.Fatalf("Got true. Want false.")
-	}
+	assertFalse(ok, t)
 }
 
 func TestShouldApplyMapToLinkedListList(t *testing.T) {
@@ -222,10 +154,7 @@ func TestShouldApplyMapToLinkedListList(t *testing.T) {
 	l.Append(1)
 	l.Map(func(i int) int { return i * 2 })
 	result, _ := l.Get(0)
-	want := 2
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 2, t)
 }
 
 func TestShouldApplyFilterToLinkedListList(t *testing.T) {
@@ -236,13 +165,7 @@ func TestShouldApplyFilterToLinkedListList(t *testing.T) {
 	l.Append(4)
 	l.Filter(func(i int) bool { return i%2 == 0 })
 	result, _ := l.Get(0)
-	want := 2
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 2, t)
 	result, _ = l.Get(1)
-	want = 4
-	if want != result {
-		t.Fatalf("Got %v. Want %v.\n", result, want)
-	}
+	assertEqual(result, 4, t)
 }
