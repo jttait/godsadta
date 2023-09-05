95bb8996fe6fbdab074bdd2433e6f7ea65d273dc
Jonathon Tait <jttait@gmail.com>
2023-09-05 08:29:55 +0100

    refactor binary search tree

--- binarysearchtree/binary_search_tree_test.go
+++ binarysearchtree/binary_search_tree_test.go
@@ -21,30 +21,26 @@ func TestShouldInsertIntoEmptyBinarySearchTree(t *testing.T) {
 }
 
 func TestShouldInsertIntoNonEmptyBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	_ = b.Insert(10)
 	result := b.Root.Right.Val
 	assert.AssertEqual(result, 10, t)
 }
 
 func TestShouldTrueWhenInsertingItemThatIsNotAlreadyInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	ok := b.Insert(10)
 	assert.AssertTrue(ok, t)
 }
 
 func TestShouldFalseWhenInsertingItemThatIsAlreadyInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	ok := b.Insert(5)
 	assert.AssertFalse(ok, t)
 }
 
 func TestShouldRemoveItemFromBinarySearchTreeOfOneNode(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	_ = b.Remove(5)
 	if b.Root != nil {
 		t.Fatalf("Root node should be nil.")
@@ -52,10 +48,7 @@ func TestShouldRemoveItemFromBinarySearchTreeOfOneNode(t *testing.T) {
 }
 
 func TestShouldRemoveItemFromBinarySearchTreeOfThreeNodes(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(2)
-	_ = b.Insert(1)
-	_ = b.Insert(3)
+	b := NewBinarySearchTree[int](2, 1, 3)
 	_ = b.Remove(3)
 	result := b.Root.Right
 	if result != nil {
@@ -70,8 +63,7 @@ func TestShouldBeTrueWhenInsertingIntoEmptyBinarySearchTree(t *testing.T) {
 }
 
 func TestShouldBeFalseWhenInsertingItemThatAlreadyExistsInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	ok := b.Insert(5)
 	assert.AssertFalse(ok, t)
 }
@@ -83,8 +75,7 @@ func TestShouldBeFalseForContainsOnEmptyBinarySearchTree(t *testing.T) {
 }
 
 func TestShouldBeTrueForContainsOnBinarySearchTreeThatContainsItem(t *testing.T) {
-	b := NewBinarySearchTree[int]()
-	_ = b.Insert(5)
+	b := NewBinarySearchTree[int](5)
 	result := b.Contains(5)
 	assert.AssertTrue(result, t)
 }
