230dca63c8b927fd5402e3b46863ed1244f266a1
Jonathon Tait <jttait@gmail.com>
2024-01-21 11:24:12 +0000

    add priority queue tests

--- maxheappriorityqueue.go
+++ maxheappriorityqueue.go
@@ -1,5 +1,5 @@
 // Package priorityqueue provides the priority queue abstract data type and associated methods
-package priorityqueue
+package godsa
 
 import (
 	"cmp"
@@ -9,31 +9,31 @@ import (
 
 // PriorityQueue is a list of elements which each have a priority. When polling the queue, the
 // highest priority item is returned.
-type PriorityQueueMaxHeap[T cmp.Ordered] struct {
+type MaxHeapPriorityQueue[T cmp.Ordered] struct {
 	maxheap maxheap.MaxHeap[T]
 }
 
 // NewPriorityQueue instantiates a priority queue and returns a pointer to it.
-func NewPriorityQueueMaxHeap[T cmp.Ordered]() *PriorityQueueMaxHeap[T] {
-	q := PriorityQueueMaxHeap[T]{}
+func NewMaxHeapPriorityQueue[T cmp.Ordered]() *MaxHeapPriorityQueue[T] {
+	q := MaxHeapPriorityQueue[T]{}
 	q.maxheap = maxheap.NewMaxHeapArray[T]()
 	return &q
 }
 
 // Size returns the number of items in the priority queue.
-func (q *PriorityQueueMaxHeap[T]) Size() int {
+func (q *MaxHeapPriorityQueue[T]) Size() int {
 	return q.maxheap.Size()
 }
 
 // Add adds an item the priority queue.
-func (q *PriorityQueueMaxHeap[T]) Insert(i T) {
+func (q *MaxHeapPriorityQueue[T]) Insert(i T) {
 	q.maxheap.Insert(i)
 }
 
 // Poll removes the highest-priority item and returns it. It returns a Boolean that is false if the
 // priority queue was empty. In cases where the priority queue was empty then the zero value of the
 // type will be returned but this is meaningless.
-func (q *PriorityQueueMaxHeap[T]) Extract() (T, bool) {
+func (q *MaxHeapPriorityQueue[T]) Extract() (T, bool) {
 	return q.maxheap.Extract()
 }
 
@@ -41,6 +41,6 @@ func (q *PriorityQueueMaxHeap[T]) Extract() (T, bool) {
 // It also returns a Boolean that is false if the priority queue was empty. In
 // cases where the priority queue was empty, the zero value for the type will be returned but this is
 // meaningless.
-func (q *PriorityQueueMaxHeap[T]) Peek() (T, bool) {
+func (q *MaxHeapPriorityQueue[T]) Peek() (T, bool) {
 	return q.maxheap.Peek()
 }
