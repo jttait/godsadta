1db7c9de741cfb753a89c1d3c4cdafb754632c0f
Jonathon Tait <jttait@gmail.com>
2024-01-21 10:49:07 +0000

    add multiset test

---
+++ multiset_test.go
@@ -0,0 +1,84 @@
+package godsa
+
+import (
+	"testing"
+
+	"github.com/jttait/godsa/assert"
+)
+
+func getMultisetImplementations() []Multiset[int] {
+	return []Multiset[int]{
+		NewMapMultiset[int](),
+	}
+}
+
+func TestShouldAddSameItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(5)
+		m.Add(5)
+		result := m.Size()
+		assert.AssertEqual(result, 3, t)
+	}
+}
+
+func TestShouldAddDifferentItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(6)
+		m.Add(7)
+		result := m.Size()
+		assert.AssertEqual(result, 3, t)
+	}
+}
+
+func TestShouldRemoveSameItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(5)
+		m.Add(5)
+		m.Remove(5)
+		m.Remove(5)
+		result := m.Size()
+		assert.AssertEqual(result, 1, t)
+	}
+}
+
+func TestShouldRemoveDifferentItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(6)
+		m.Add(7)
+		m.Remove(6)
+		m.Remove(7)
+		result := m.Size()
+		assert.AssertEqual(result, 1, t)
+	}
+}
+
+func TestShouldCountSameItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(5)
+		m.Add(5)
+		result := m.Count(5)
+		assert.AssertEqual(result, 3, t)
+	}
+}
+
+func TestShouldCountDifferentItemsToMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		m.Add(5)
+		m.Add(6)
+		m.Add(7)
+		result := m.Count(6)
+		assert.AssertEqual(result, 1, t)
+	}
+}
+
+func TestShouldCountZeroForItemThatIsNotInMultiset(t *testing.T) {
+	for _, m := range getMultisetImplementations() {
+		result := m.Count(6)
+		assert.AssertEqual(result, 0, t)
+	}
+}
