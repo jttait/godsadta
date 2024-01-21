3692229787805123511e3318461b9439c4f8eb59
Jonathon Tait <jttait@gmail.com>
2024-01-21 10:42:26 +0000

    add stack test

---
+++ stack_test.go
@@ -0,0 +1,74 @@
+package godsa
+
+import (
+	"testing"
+
+	"github.com/jttait/godsa/assert"
+)
+
+func getStackImplementationsInt() []Stack[int] {
+	return []Stack[int]{
+		NewLLStack[int](),
+	}
+}
+
+func getStackImplementationsString() []Stack[string] {
+	return []Stack[string]{
+		NewLLStack[string](),
+	}
+}
+
+func TestShouldBeSizeZeroForNewlyInstantiatedStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		result := stack.Size()
+		assert.AssertEqual(result, 0, t)
+	}
+}
+
+func TestShouldBeSizeOneForItemPushedToNewlyInstantiatedStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		stack.Push(5)
+		result := stack.Size()
+		assert.AssertEqual(result, 1, t)
+	}
+}
+
+func TestShouldPopLatestPushedIntegerFromStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		stack.Push(5)
+		result, ok := stack.Pop()
+		assert.AssertTrue(ok, t)
+		assert.AssertEqual(result, 5, t)
+	}
+}
+
+func TestShouldPopLatestPushedStringFromStack(t *testing.T) {
+	for _, stack := range getStackImplementationsString() {
+		stack.Push("five")
+		result, ok := stack.Pop()
+		assert.AssertTrue(ok, t)
+		assert.AssertEqual(result, "five", t)
+	}
+}
+
+func TestShouldReturnFalseWhenPoppingEmptyStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		_, ok := stack.Pop()
+		assert.AssertFalse(ok, t)
+	}
+}
+
+func TestShouldReturnLatestItemWhenPeekingStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		stack.Push(5)
+		result, _ := stack.Peek()
+		assert.AssertEqual(result, 5, t)
+	}
+}
+
+func TestShouldReturnFalseWhenPeekingEmptyStack(t *testing.T) {
+	for _, stack := range getStackImplementationsInt() {
+		_, ok := stack.Peek()
+		assert.AssertFalse(ok, t)
+	}
+}
