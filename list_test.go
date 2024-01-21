f0f42f81a48333f617cb8b370da8cf8f10b2909c
Jonathon Tait <jttait@gmail.com>
2024-01-21 09:34:59 +0000

    rename tests

--- list_test.go
+++ list_test.go
@@ -13,14 +13,14 @@ func getImplementations() []List[int] {
 	}
 }
 
-func TestShouldInstantiateLLListOfSizeZero(t *testing.T) {
+func TestShouldInstantiateListOfSizeZero(t *testing.T) {
 	for _, l := range getImplementations() {
 		result := l.Size()
 		assert.AssertEqual(result, 0, t)
 	}
 }
 
-func TestShouldAppendMultipleItemsToLLList(t *testing.T) {
+func TestShouldAppendMultipleItemsToList(t *testing.T) {
 	l := NewLLList[int]()
 	l.Append(5)
 	l.Append(6)
@@ -29,7 +29,7 @@ func TestShouldAppendMultipleItemsToLLList(t *testing.T) {
 	assert.AssertTrue(l.Equal(want), t)
 }
 
-func TestShouldPrependMultipleItemsToLLList(t *testing.T) {
+func TestShouldPrependMultipleItemsToList(t *testing.T) {
 	l := NewLLList[int]()
 	l.Prepend(5)
 	l.Prepend(6)
@@ -38,89 +38,89 @@ func TestShouldPrependMultipleItemsToLLList(t *testing.T) {
 	assert.AssertTrue(l.Equal(want), t)
 }
 
-func TestShouldBeFalseIfGetOnEmptyLLList(t *testing.T) {
+func TestShouldBeFalseIfGetOnEmptyList(t *testing.T) {
 	l := NewLLList[int]()
 	_, ok := l.Get(0)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldBeTrueIfGetIndexIsWithinLLList(t *testing.T) {
+func TestShouldBeTrueIfGetIndexIsWithinList(t *testing.T) {
 	l := NewLLList[int](5)
 	_, ok := l.Get(0)
 	assert.AssertTrue(ok, t)
 }
 
-func TestShouldBeFalseIfGetIndexIsOutsideLLList(t *testing.T) {
+func TestShouldBeFalseIfGetIndexIsOutsideList(t *testing.T) {
 	l := NewLLList[int](5)
 	_, ok := l.Get(1)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldRemoveItemAtIndexOfLLList(t *testing.T) {
+func TestShouldRemoveItemAtIndexOfList(t *testing.T) {
 	l := NewLLList[int](1, 2, 3)
 	_ = l.Remove(1)
 	result, _ := l.Get(1)
 	assert.AssertEqual(result, 3, t)
 }
 
-func TestShouldBeFalseWhenRemovingFromEmptyLLList(t *testing.T) {
+func TestShouldBeFalseWhenRemovingFromEmptyList(t *testing.T) {
 	l := NewLLList[int]()
 	ok := l.Remove(0)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldBeFalseWhenRemovingFromIndexOutsideLLList(t *testing.T) {
+func TestShouldBeFalseWhenRemovingFromIndexOutsideList(t *testing.T) {
 	l := NewLLList[int](5)
 	ok := l.Remove(1)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldBeTrueWhenRemovingFromIndexInsideLLList(t *testing.T) {
+func TestShouldBeTrueWhenRemovingFromIndexInsideList(t *testing.T) {
 	l := NewLLList[int](5)
 	ok := l.Remove(0)
 	assert.AssertTrue(ok, t)
 }
 
-func TestShouldInsertItemAtIndexOfLLList(t *testing.T) {
+func TestShouldInsertItemAtIndexOfList(t *testing.T) {
 	l := NewLLList[int](1)
 	_ = l.Insert(0, 2)
 	result, _ := l.Get(0)
 	assert.AssertEqual(result, 2, t)
 }
 
-func TestShouldShiftTheItemCurrentlyAtTheIndexToRightWithLLList(t *testing.T) {
+func TestShouldShiftTheItemCurrentlyAtTheIndexToRightWithList(t *testing.T) {
 	l := NewLLList[int](1)
 	_ = l.Insert(0, 2)
 	result, _ := l.Get(1)
 	assert.AssertEqual(result, 1, t)
 }
 
-func TestShouldBeFalseWhenInsertingIntoEmptyLLList(t *testing.T) {
+func TestShouldBeFalseWhenInsertingIntoEmptyList(t *testing.T) {
 	l := NewLLList[int]()
 	ok := l.Insert(0, 1)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldBeTrueWhenInsertingIntoLLList(t *testing.T) {
+func TestShouldBeTrueWhenInsertingIntoList(t *testing.T) {
 	l := NewLLList[int](1)
 	ok := l.Insert(0, 2)
 	assert.AssertTrue(ok, t)
 }
 
-func TestShouldBeFalseWhenInsertingIntoIndexOutsideLLList(t *testing.T) {
+func TestShouldBeFalseWhenInsertingIntoIndexOutsideList(t *testing.T) {
 	l := NewLLList[int](1)
 	ok := l.Insert(1, 2)
 	assert.AssertFalse(ok, t)
 }
 
-func TestShouldApplyMapToLLList(t *testing.T) {
+func TestShouldApplyMapToList(t *testing.T) {
 	l := NewLLList[int](1, 2, 3)
 	l.Map(func(i int) int { return i * 2 })
 	want := NewLLList[int](2, 4, 6)
 	assert.AssertTrue(l.Equal(want), t)
 }
 
-func TestShouldApplyFilterToLLList(t *testing.T) {
+func TestShouldApplyFilterToList(t *testing.T) {
 	l := NewLLList[int](1, 2, 3, 4)
 	l.Filter(func(i int) bool { return i%2 == 0 })
 	want := NewLLList[int](2, 4)
