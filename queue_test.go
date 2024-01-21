234440c87cffe94ebee0f694c3f9af161287d22f
Jonathon Tait <jttait@gmail.com>
2024-01-21 10:33:39 +0000

    dequeue test

--- queue_test.go
+++ queue_test.go
@@ -6,72 +6,86 @@ import (
 	"github.com/jttait/godsa/assert"
 )
 
-func getQueueImplementations() []Queue[int] {
+func getQueueImplementationsInt() []Queue[int] {
 	return []Queue[int]{
 		NewLLQueue[int](),
 	}
 }
 
+func getQueueImplementationsString() []Queue[string] {
+	return []Queue[string]{
+		NewLLQueue[string](),
+	}
+}
+
 func TestShouldBeSizeZeroForNewlyInstantiatedQueue(t *testing.T) {
-	q := NewLLQueue[int]()
-	result := q.Size()
-	assert.AssertEqual(result, 0, t)
+	for _, q := range getQueueImplementationsInt() {
+		result := q.Size()
+		assert.AssertEqual(result, 0, t)
+	}
 }
 
 func TestShouldBeSizeOneWhenItemAddedToNewlyInstantiatedQueue(t *testing.T) {
-	q := NewLLQueue[int]()
-	q.Insert(5)
-	result := q.Size()
-	assert.AssertEqual(result, 1, t)
+	for _, q := range getQueueImplementationsInt() {
+		q.Insert(5)
+		result := q.Size()
+		assert.AssertEqual(result, 1, t)
+	}
 }
 
 func TestShouldRemoveItemFromQueueOfSizeOne(t *testing.T) {
-	q := NewLLQueue[int]()
-	q.Insert(5)
-	result, _ := q.Remove()
-	assert.AssertEqual(result, 5, t)
+	for _, q := range getQueueImplementationsInt() {
+		q.Insert(5)
+		result, _ := q.Remove()
+		assert.AssertEqual(result, 5, t)
+	}
 }
 
 func TestShouldBeFalseWhenRemovingFromEmptyQueue(t *testing.T) {
-	q := NewLLQueue[int]()
-	_, ok := q.Remove()
-	assert.AssertFalse(ok, t)
+	for _, q := range getQueueImplementationsInt() {
+		_, ok := q.Remove()
+		assert.AssertFalse(ok, t)
+	}
 }
 
 func TestShouldBeTrueWhenRemovingFromNonEmptyQueue(t *testing.T) {
-	q := NewLLQueue[int]()
-	q.Insert(5)
-	_, ok := q.Remove()
-	assert.AssertTrue(ok, t)
+	for _, q := range getQueueImplementationsInt() {
+		q.Insert(5)
+		_, ok := q.Remove()
+		assert.AssertTrue(ok, t)
+	}
 }
 
 func TestShouldRemoveIntegerThatWasAddedEarliestFromQueue(t *testing.T) {
-	q := NewLLQueue[int]()
-	q.Insert(5)
-	q.Insert(6)
-	result, _ := q.Remove()
-	assert.AssertEqual(result, 5, t)
+	for _, q := range getQueueImplementationsInt() {
+		q.Insert(5)
+		q.Insert(6)
+		result, _ := q.Remove()
+		assert.AssertEqual(result, 5, t)
+	}
 }
 
 func TestShouldRemoveStringThatWasAddedEarliestFromQueue(t *testing.T) {
-	q := NewLLQueue[string]()
-	q.Insert("five")
-	q.Insert("six")
-	result, _ := q.Remove()
-	assert.AssertEqual(result, "five", t)
+	for _, q := range getQueueImplementationsString() {
+		q.Insert("five")
+		q.Insert("six")
+		result, _ := q.Remove()
+		assert.AssertEqual(result, "five", t)
+	}
 }
 
 func TestShouldRemoveMultipleStringsFromQueue(t *testing.T) {
-	q := NewLLQueue[string]()
-	q.Insert("five")
-	q.Insert("six")
-	q.Insert("seven")
-	result, _ := q.Remove()
-	assert.AssertEqual(result, "five", t)
-	result, _ = q.Remove()
-	assert.AssertEqual(result, "six", t)
-	result, _ = q.Remove()
-	assert.AssertEqual(result, "seven", t)
-	_, ok := q.Remove()
-	assert.AssertFalse(ok, t)
+	for _, q := range getQueueImplementationsString() {
+		q.Insert("five")
+		q.Insert("six")
+		q.Insert("seven")
+		result, _ := q.Remove()
+		assert.AssertEqual(result, "five", t)
+		result, _ = q.Remove()
+		assert.AssertEqual(result, "six", t)
+		result, _ = q.Remove()
+		assert.AssertEqual(result, "seven", t)
+		_, ok := q.Remove()
+		assert.AssertFalse(ok, t)
+	}
 }
