4d61ab1bb523e126e2f7145527680a9c4d2f60df
Jonathon Tait <jttait@gmail.com>
2024-01-21 08:43:00 +0000

    refactor stack

---
+++ stack/llstack_test.go
@@ -0,0 +1,55 @@
+package stack
+
+import (
+	"testing"
+
+	"github.com/jttait/godsa/assert"
+)
+
+func TestShouldBeSizeZeroForNewlyInstantiatedLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	result := stack.Size()
+	assert.AssertEqual(result, 0, t)
+}
+
+func TestShouldBeSizeOneForItemPushedToNewlyInstantiatedLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	stack.Push(5)
+	result := stack.Size()
+	assert.AssertEqual(result, 1, t)
+}
+
+func TestShouldPopLatestPushedIntegerFromLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	stack.Push(5)
+	result, ok := stack.Pop()
+	assert.AssertTrue(ok, t)
+	assert.AssertEqual(result, 5, t)
+}
+
+func TestShouldPopLatestPushedStringFromLLStack(t *testing.T) {
+	stack := NewLLStack[string]()
+	stack.Push("five")
+	result, ok := stack.Pop()
+	assert.AssertTrue(ok, t)
+	assert.AssertEqual(result, "five", t)
+}
+
+func TestShouldReturnFalseWhenPoppingEmptyLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	_, ok := stack.Pop()
+	assert.AssertFalse(ok, t)
+}
+
+func TestShouldReturnLatestItemWhenPeekingLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	stack.Push(5)
+	result, _ := stack.Peek()
+	assert.AssertEqual(result, 5, t)
+}
+
+func TestShouldReturnFalseWhenPeekingEmptyLLStack(t *testing.T) {
+	stack := NewLLStack[int]()
+	_, ok := stack.Peek()
+	assert.AssertFalse(ok, t)
+}
