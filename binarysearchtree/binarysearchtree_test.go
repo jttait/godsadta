3546746b194fada74786814070e81b3385eee154
Jonathon Tait <jttait@gmail.com>
2024-02-18 19:56:04 +0000

    refactor binarysearchtree

--- binarysearchtree/binarysearchtree_test.go
+++ binarysearchtree/binarysearchtree_test.go
@@ -7,40 +7,40 @@ import (
 )
 
 func TestShouldInstantiateBinarySearchTreeWithNilRootNode(t *testing.T) {
-	b := NewBinarySearchTree[int]()
+	b := NewUnbalancedBinarySearchTree[int]()
 	if b.Root != nil {
 		t.Fatalf("Root node is not nil.")
 	}
 }
 
 func TestShouldInsertIntoEmptyBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
+	b := NewUnbalancedBinarySearchTree[int]()
 	_ = b.Insert(5)
 	result := b.Root.Val
 	assert.AssertEqual(result, 5, t)
 }
 
 func TestShouldInsertIntoNonEmptyBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	_ = b.Insert(10)
 	result := b.Root.Right.Val
 	assert.AssertEqual(result, 10, t)
 }
 
 func TestShouldTrueWhenInsertingItemThatIsNotAlreadyInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	ok := b.Insert(10)
 	assert.AssertTrue(ok, t)
 }
 
 func TestShouldFalseWhenInsertingItemThatIsAlreadyInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	ok := b.Insert(5)
 	assert.AssertFalse(ok, t)
 }
 
 func TestShouldRemoveItemFromBinarySearchTreeOfOneNode(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	_ = b.Remove(5)
 	if b.Root != nil {
 		t.Fatalf("Root node should be nil.")
@@ -48,7 +48,7 @@ func TestShouldRemoveItemFromBinarySearchTreeOfOneNode(t *testing.T) {
 }
 
 func TestShouldRemoveItemFromBinarySearchTreeOfThreeNodes(t *testing.T) {
-	b := NewBinarySearchTree[int](2, 1, 3)
+	b := NewUnbalancedBinarySearchTree[int](2, 1, 3)
 	_ = b.Remove(3)
 	result := b.Root.Right
 	if result != nil {
@@ -57,25 +57,25 @@ func TestShouldRemoveItemFromBinarySearchTreeOfThreeNodes(t *testing.T) {
 }
 
 func TestShouldBeTrueWhenInsertingIntoEmptyBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
+	b := NewUnbalancedBinarySearchTree[int]()
 	ok := b.Insert(5)
 	assert.AssertTrue(ok, t)
 }
 
 func TestShouldBeFalseWhenInsertingItemThatAlreadyExistsInBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	ok := b.Insert(5)
 	assert.AssertFalse(ok, t)
 }
 
 func TestShouldBeFalseForContainsOnEmptyBinarySearchTree(t *testing.T) {
-	b := NewBinarySearchTree[int]()
+	b := NewUnbalancedBinarySearchTree[int]()
 	result := b.Contains(5)
 	assert.AssertFalse(result, t)
 }
 
 func TestShouldBeTrueForContainsOnBinarySearchTreeThatContainsItem(t *testing.T) {
-	b := NewBinarySearchTree[int](5)
+	b := NewUnbalancedBinarySearchTree[int](5)
 	result := b.Contains(5)
 	assert.AssertTrue(result, t)
 }
